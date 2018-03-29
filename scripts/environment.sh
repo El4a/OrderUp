3e77aea930a9244d32eaa28082d2f68d695fb3f3
El4a <elvira.poiters@gmail.com>
2018-03-29 11:50:37 +0200

    New bin

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 export JSONBIN_API_TOKEN="\$2a\$10\$9mNMmTmhL37eGDo3Eeiac.me76D6aZuw/J9pg4lRrr9o.znKfOelq"
-export LOCAL_BIN_ID="5ab7ce352efae41465ebc12c"
+export LOCAL_BIN_ID="5abb4992daaaea147dca8798"
 export PRD_BIN_ID="5ab7ce6adaaaea147dca83ca"
-
