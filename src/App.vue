56148e1c2f3ce6bc82fc56641e73b25106659add
El4a <elvira.poiters@gmail.com>
2018-03-29 11:53:40 +0200

    fix buggy css on mobile for navigation swipes

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
      <md-tabs md-sync-route md-alignment="fixed">
-        <md-tab md-label="Drinks" to="/"></md-tab>
-        <md-tab md-label="Orders" to="/orders"></md-tab>
-        <md-tab md-label="FAQ" to="/faq"></md-tab>
+        <md-tab md-label="Drinks" to="/" id='tab_drinks' exact></md-tab>
+        <md-tab md-label="Orders" to="/orders" id='tab_orders' exact></md-tab>
+        <md-tab md-label="FAQ" to="/faq" id='tab_faq' exact></md-tab>
       </md-tabs>
       <!-- TODO optional : custom template for amount of orders?-->
     <router-view/>
@@ -21,10 +21,18 @@
 
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   #app {
     background-color: rgba(233,233,233, 1);
     min-height: 100vh;
+  }
+
+  //krijg de material css die de foute highlight in de tab veroorzaakt niet overschreven dus gebruik deze css klass van vue-router als hook
+  .router-link-active:hover:before {
+    background-color:#e9e9e9 !important;
+  }
+  .router-link-active:before {
+    background-color: transparent !important;
   }
 
 </style>
