9939e6f94df288d3f221089af5a4aeff870aaa8a
El4a <elvira.poiters@gmail.com>
2018-03-29 14:36:37 +0200

    redirect homepage to avoid using / path because of vue material route bug

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
      <md-tabs md-sync-route md-alignment="fixed">
-        <md-tab md-label="Drinks" to="/" id='tab_drinks' exact></md-tab>
-        <md-tab md-label="Orders" to="/orders" id='tab_orders' exact></md-tab>
-        <md-tab md-label="FAQ" to="/faq" id='tab_faq' exact></md-tab>
+        <md-tab md-label="Drinks" to="/drinks" id='tab_drinks'></md-tab>
+        <md-tab md-label="Orders" to="/orders" id='tab_orders'></md-tab>
+        <md-tab md-label="FAQ" to="/faq" id='tab_faq'></md-tab>
       </md-tabs>
       <!-- TODO optional : custom template for amount of orders?-->
     <router-view/>
@@ -28,12 +28,12 @@
   }
 
   //krijg de material css die de foute highlight in de tab veroorzaakt niet overschreven dus gebruik deze css klass van vue-router als hook
-  .router-link-active:hover:before {
-    background-color:#e9e9e9 !important;
-  }
-  .router-link-active:before {
-    background-color: transparent !important;
-  }
+  // .router-link-active:hover:before {
+  //   background-color:#e9e9e9 !important;
+  // }
+  // .router-link-active:before {
+  //   background-color: transparent !important;
+  // }
 
 </style>
 
--- src/router/index.js
+++ src/router/index.js
@@ -10,8 +10,12 @@ export default new Router({
   routes: [
     {
       path: '/',
+      redirect: '/drinks'
+    },
+    {
+      path: '/drinks',
       name: 'drinks', //also doesnt seem necessary BUT is saved on the $this.route object so useful for routing "hacks" (see swipecommands for navigation on mobile)
-      component: Drinks
+      component: Drinks,
     },
     {
       path: "/orders",
