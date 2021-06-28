f3adbb8b3d9d518ef93c5913ff3c9fdfdfdf62e9
El4a <El4a@users.noreply.github.com>
2021-06-28 13:04:16 +0100

    Update DrinkService.vue

--- src/data/DrinkService.vue
+++ src/data/DrinkService.vue
@@ -2,78 +2,78 @@
   const drinks = [
     {
       id: 1,
-      name: 'Pintje',
-      description: "Simpel, dorstlessend, traditioneel. Jupiler, apache-helicopters weten waarom.",
+      name: 'A beer',
+      description: "Simple, thirst quenching, traditional, manly.",
       img: 'pintje.jpg',
       ingredients: ['water', 'hop', '???']
     },
     {
       id: 2,
       name: 'Duvel',
-      description: "'Pff pintjes zijn voor de zwakkelingen. Ik drink enkel Duvel, zoals échte venten' - De typische loser die direct plat gaat.",
+      description: "'Pff regular beer is for the weak. I only drink heavy beers, like réal men.' - The typical loser who can't handle their liquor.",
       img: 'duvel.jpg',
-      ingredients: ['water', 'thee']
+      ingredients: ['water', 'strong hop']
     },
     {
       id: 3,
-      name: 'Thee',
-      description: "Warm water met een smaakje. Standaard: Rooibos! Voor andere opties (honeybush, earl grey, iets men mango): come see me.",
+      name: 'Tea',
+      description: "Flavoured warm water, yum! Default: Rooibos! For other options (honeybush, earl grey, something mango): come see me.",
       img: 'tea.jpeg',
-      ingredients: ['water', 'thee']
+      ingredients: ['water', 'herbs']
     },
     {
       id: 4,
-      name: 'Koffie',
-      description: "'t Is echt héél brakke (oplos)koffie, maar meh caffeine I guess.",
+      name: 'Coffee',
+      description: "'Extremely crap but hey, it has caffeine!",
       img: 'coffee.jpeg',
-      ingredients: ['water', 'koffie']
+      ingredients: ['water', 'coffee beans', 'milk (optional)']
     },
     {
       id: 5,
-      name: 'Witte wijn',
-      description: "Als de wijn gaet in den man, Leyt de wijsheyt in de kan.",
+      name: 'White wine',
+      description: "Atleast when you spill this, it doesn't leave a stain.",
       img: 'white-wine.jpg',
-      ingredients: ['druivennat']
+      ingredients: ['grapes']
     },
     {
       id: 6,
-      name: 'Rode wijn',
-      description: "Zoals witte wijn, maar beter.",
+      name: 'Red wine',
+      description: "Like white wine, but more advanced.",
       img: 'red-wine.jpeg',
-      ingredients: ['druivennat', 'druivenschil']
+      ingredients: ['grapes', 'grape peels']
     },
     {
       id: 7,
       name: 'Cava',
-      description: "Bubbels! Nee Lisa sorry, 't is geen echte champagne.",
+      description: "Bubbels! No Lisa, ffs, it's not 'real' champagne. Deal with it.",
       img: 'cava.png',
-      ingredients: ['???']
+      ingredients: ['also water and grapes I guess?', 'gas somehow']
     },
     {
       id: 8,
       name: 'Mimosa',
-      description: "Het sociaal aanvaardbare drankje om 11u 's morgens.",
+      description: "The socially accepted beverage at 10am.",
       img: 'mimosa.jpg',
       ingredients: ['Cava', 'Sinaasappelsap']
     },
     {
       id: 9,
       name: 'Wodka Orange',
-      description: "Voor dat extra beetje vitamine C.",
+      description: "If you need some vitamine C.",
       img: 'screwdriver.jpg',
-      ingredients: ['Wodka', 'Sinaasappelsap']
+      ingredients: ['Wodka', 'OJ']
     },
     {
       id: 10,
       name: 'Cuba Libre',
-      description: "",
+      description: "A classic and you don't even need to taste the alcohol.",
       img: 'cuba-libre.jpg',
-      ingredients: ['Bruine rum', 'Cola']
+      ingredients: ['Brown rum', 'Coke']
     },
     {
       id: 11,
       name: 'Gin Tonic',
-      description: "Aah, de hype van 2014. Maar volgens HLN zet deze trend zich ook komende zomer weer verder!",
+      description: "Aah, the hype of 2014. But no worries that you still drink it, it doesn't make you unfashionable. At all.",
       img: 'gin-tonic.jpg',
       ingredients: ['Gin', 'Tonic']
     },
@@ -82,35 +82,35 @@
       name: 'Mojito',
       description: "",
       img: 'mojito.jpg',
-      ingredients: ['Witte rum', 'Munt', 'Rietsuiker', 'Limoen', 'Bruiswater']
+      ingredients: ['White rum', 'Mint', 'Sugar', 'Lime', 'Soda']
     },
     {
       id: 13,
       name: 'Caprioska',
-      description: "Niet te verwarren met een Caipirinha.",
+      description: "Not to be confused with a Caipirinha.",
       img: 'caprioska.jpg',
-      ingredients: ['Wodka', 'Limoen', 'Rietsuiker']
+      ingredients: ['Wodka', 'Lime', 'Sugar']
     },
     {
       id: 14,
       name: 'Caipirinha',
-      description: "Niet te verwarren met een Caprioska.",
+      description: "Not to be confused with aCaprioska.",
       img: 'caipirinha.jpeg',
-      ingredients: ['Witte rum', 'Limoen', 'Rietsuiker']
+      ingredients: ['White rum', 'Lime', 'Sugar']
     },
     {
       id: 15,
       name: 'Margarita',
-      description: "Niet de pizza.",
+      description: "Not the pizza.",
       img: 'margarita.jpg',
-      ingredients: ['Tequila', 'Triple sec', 'Limoen']
+      ingredients: ['Tequila', 'Triple sec', 'Lime']
     },
     {
       id: 16,
       name: 'Long Island Iced Tea',
-      description: "Voor zij die van plan zijn te blijven slapen. Bestaat uit 40% alcohol en 60% kater.",
+      description: "A personal favorite. For those who aren't planning to walk nor see straight. Consists of 40% alcohol and 60% hangover.",
       img: 'long-island-ice-tea.jpeg',
-      ingredients: ['Wodka', 'Gin', 'Rum', 'Tequila', 'Triple Sec', 'Citroen', 'Cola']
+      ingredients: ['Wodka', 'Gin', 'Rum', 'Tequila', 'Triple Sec', 'Lemon', 'Coke']
     }
   ];
   export default {
